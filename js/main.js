561f644cbd45fdd45fd3bb09524d05cadaf56da9
rincedal <94070451+Diomuzan247@users.noreply.github.com>
2023-04-06 19:07:46 +0200

    Migration of repo completed

---
+++ js/main.js
@@ -0,0 +1,33 @@
+console.log('Main file is loaded!');
+let season = "Winter"
+// Ik heb voor een let gekozen omdat de antwoord, in dit geval de seizoenen kunnen gaan veranderen
+const name = prompt('Hallo {$name}, wat is jouw favoriete seizoen?')
+// Ik heb hier voor een const gekozen omdat de favoriete sezioen van iemand meestal niet verandert
+
+if (season = "Winter") {
+alert('Dagje Scheveningen, lekker bakken in de zon!');
+
+} else if (season = "Herfst") {
+alert('In de herfst kan je prima barbecueën!');
+}
+
+let wind = prompt('Wat is de windkracht momenteel?');
+// Ik heb hier voor een let gekozen omdat de windkracht de hele tijd kan veranderen
+let umbrella = prompt('Heb je een paraplu bij je?');
+
+if (wind < 6) {
+prompt('Er is geen vuiltje aan de lucht');
+} else if ( wind < 9 === umbrella = 'Ja') {
+prompt('Het is oppassen geblazen');
+} else {
+prompt('Blijf binnen als je de deur niet uit hoeft!');
+}
+const day = [1, 2, 3, 4, 5, 6, 7]
+// Ik heb voor een const gekozen omdat de aantal dagen van de week niet verandert.
+
+let rain = (1 * 7);
+// Ik heb hier voor een let gekozen de som kan veranderen.
+console.log(rain);
+
+
+
